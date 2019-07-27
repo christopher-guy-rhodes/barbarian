e88e6d02d9270e26cb4dce7ffc42fc2ebca5878c
Rhodes <crhodes@amazon.com>
2019-07-26 21:30:13 -0700

    Introduced animate() method for monster animation

--- actions.js
+++ actions.js
@@ -64,52 +64,8 @@ function actionHelper(direction, isRunning, frames, requestedAction, times = 0) 
     animateSprite(barbarian, 'barbarian',  frames[direction]['FRAMES'], requestedAction, isRight ? RIGHT : LEFT, frames[direction]['HEIGHT_OFFSET'], times);
 }
 
-// TODP: break out into monster module
 async function monsterAttack() {
 
-    monster.css('background-position', -1*14*monster.width() + 'px ' + -1*monster.height() + 'px');
-    var index = 0;
-    var path = WALK_FRAMES[LEFT]['FRAMES'];
-    var distance = windowWidth;
-    monster.animate({left: (monster.offset().left - distance) + 'px'}, distance / SPRITE_PIXELS_PER_SECOND * 1000, 'linear');
-    while(true) {
-        
-        frame = path[index];
-        monster.css('background-position',(-1*frame*monster.width()) + 'px ' + -1*monster.height()*WALK_FRAMES[LEFT]['HEIGHT_OFFSET'] + 'px');
-        index++;
-        if (index == path.length) {
-            index = 0;
-        }
-        //console.log('barbarian:' + barbarian.offset().left + ' monster:' + monster.offset().left);
-        if (monster.offset().left - barbarian.offset().left < 200) {
-            monster.stop();
-            break;
-        }
-        await sleep(1000/SPRITE_FPS);
-    }
-
-    index = 0;
-    path = ATTACK_FRAMES[LEFT]['FRAMES'];
-    distance = 200;
-    monsterAttackTime = new Date().getTime();
-    var barbarianAttackDistance = monster.offset().left - barbarian.offset().left;
-    console.log('barbarian attack time:' + (barbarianAttackTime - monsterAttackTime) + ' distance:' + barbarianAttackDistance);
-
-    for (index in path) {
-        console.log(index);
-        if (index == 2) {
-            console.log('now');
-            if (barbarianAttackDistance < 200 && barbarianAttackTime < monsterAttackTime) {
-                monster.css('display','none');
-                monster.stop();
-            } else {
-                barbarian.css('display','none');
-                barbarian.stop();
-            }
-        }
-        frame = path[index];
-        monster.css('background-position',(-1*frame*monster.width()) + 'px ' + -1*monster.height()*ATTACK_FRAMES[LEFT]['HEIGHT_OFFSET'] + 'px');
-        await sleep(1000/SPRITE_FPS);
-    }
-    
+    await animate(monster, windowWidth, WALK_FRAMES[LEFT]['FRAMES'], WALK_FRAMES[LEFT]['HEIGHT_OFFSET'], true, 200);
+    await animate(monster, 0, ATTACK_FRAMES[LEFT]['FRAMES'], ATTACK_FRAMES[LEFT]['HEIGHT_OFFSET'], false, 0, true);
 }
--- animator.js
+++ animator.js
@@ -39,56 +39,96 @@
         barbarian.animate({left: '0px'}, distance / SPRITE_PIXELS_PER_SECOND / RUN_SPEED_INCREASE_FACTOR * 1000, 'linear');
     }
 
-    /**
-     * This method animates the position of the spite according to the path provided.
-     *
-     * @sprite The sprite to animate
-     * @path The path that the sprite will take.
-     * @requestedAction The action requested that is assicated with the path.
-     * @requestedDirection The direction the sprite is to move
-     @ @heightOffsetGridUnits the height offset for the sprite
-     */
-    async function animateSprite(sprite, spriteName, path, requestedAction, requestedDirection, heightOffsetGridUnits, times = 0) {
-        action[spriteName] = requestedAction;
-        direction[spriteName] = requestedDirection;
-        var heightOffset = heightOffsetGridUnits * barbarian.height(); 
-        var index = 0;
-        var iterations = times;
-        while(action[spriteName] === requestedAction && direction[spriteName] === requestedDirection) {
-           //console.log('action:' + action[spriteName] + ' requestedAction:' + requestedAction +  ' direction:' + direction[spriteName] + ' requestedDirection:' + requestedDirection);
-	   var windowWidth = $( document ).width() - barbarian.width();
-           var position = path[index];
+/**
+ * This method animates the position of the spite according to the path provided.
+ *
+ * @sprite The sprite to animate
+ * @path The path that the sprite will take.
+ * @requestedAction The action requested that is assicated with the path.
+ * @requestedDirection The direction the sprite is to move
+ * @heightOffsetGridUnits the height offset for the sprite
+ */
+async function animateSprite(sprite, spriteName, path, requestedAction, requestedDirection, heightOffsetGridUnits, times = 0) {
+    action[spriteName] = requestedAction;
+    direction[spriteName] = requestedDirection;
+    var heightOffset = heightOffsetGridUnits * barbarian.height(); 
+    var index = 0;
+    var iterations = times;
+    while(action[spriteName] === requestedAction && direction[spriteName] === requestedDirection) {
+       //console.log('action:' + action[spriteName] + ' requestedAction:' + requestedAction +  ' direction:' + direction[spriteName] + ' requestedDirection:' + requestedDirection);
+       var windowWidth = $( document ).width() - barbarian.width();
+       var position = path[index];
 
-           sprite.css('background-position',(-1*position*barbarian.width()) + 'px ' + -1*heightOffset + 'px');
-           if (action[spriteName] === STOP) {
-               break;
-           }
-           if (direction[spriteName] === LEFT && sprite.offset().left === 0) {
-               action[spriteName] = STOP;
-               break;
-           }
-           if (direction[spriteName] === RIGHT && sprite.offset().left === windowWidth) {
-               action[spriteName] = STOP;
-               break;
-           }
-           await sleep(1000/SPRITE_FPS);
+       sprite.css('background-position',(-1*position*barbarian.width()) + 'px ' + -1*heightOffset + 'px');
+       if (action[spriteName] === STOP) {
+           break;
+       }
+       if (direction[spriteName] === LEFT && sprite.offset().left === 0) {
+           action[spriteName] = STOP;
+           break;
+       }
+       if (direction[spriteName] === RIGHT && sprite.offset().left === windowWidth) {
+           action[spriteName] = STOP;
+           break;
+       }
+       await sleep(1000/SPRITE_FPS);
 
-           // loop the sprite animation
-           index++;
-           if (index == path.length) {
-               if (times < 1 || --iterations > 0) {
-                   index = 0;
-               } else {
-                   break;
-               }
-           } 
+       // loop the sprite animation
+       index++;
+       if (index == path.length) {
+           if (times < 1 || --iterations > 0) {
+               index = 0;
+           } else {
+               break;
+           }
+       } 
+    }
+
+    // jump actions might still be going on. They are the only action that moves but needs to terminate
+    if (action[spriteName] === JUMP) {
+        console.log('stop the barbarian!');
+        barbarian.stop();
+    }
+}
+
+async function animate(sprite, distance, frames, heightOffset, repeat = false, proximityStop = 0, attacking = false) {
+    var index = 0;
+    sprite.animate({left: (sprite.offset().left - distance) + 'px'}, distance / SPRITE_PIXELS_PER_SECOND * 1000, 'linear');
+    while(true) {
+        
+        frame = frames[index];
+        sprite.css('background-position',(-1*frame*sprite.width()) + 'px ' + -1*sprite.height()*heightOffset + 'px');
+        if (proximityStop > 0 && sprite.offset().left - barbarian.offset().left < proximityStop) {
+            break;
         }
 
-        // jump actions might still be going on. They are the only action that moves but needs to terminate
-        if (action[spriteName] === JUMP) {
-            console.log('stop the barbarian!');
-            barbarian.stop();
+        var barbarianAttackDistance = monster.offset().left - barbarian.offset().left;
+        if (attacking && index == 2) { 
+            if (barbarianAttackDistance < 200 && barbarianAttackTime < monsterAttackTime) {
+                sprite.css('display','none');
+                sprite.stop();
+            } else {
+                barbarian.css('display','none');
+                barbarian.stop();
+            }
+        }
+        if (index == frames.length - 1 && !repeat) {
+            console.log('shopping');
+            break;
+        } else {
+            console.log('not shopping');
+        }
+        await sleep(1000/SPRITE_FPS);
+        index++;
+
+        if (index == frames.length ) {
+            index = 0;
         }
     }
+
+    sprite.stop();
+
+}
+
 
 

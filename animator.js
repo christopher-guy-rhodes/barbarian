c67f41f367e5f76201033e2bb1d35d170be21300
Christopher Rhodes <crhodes@amazon.com>
2020-07-25 23:45:25 -0700

    Stopping at proper walk first frame position

--- animator.js
+++ animator.js
@@ -12,7 +12,7 @@
      * Move the barbarian right
      */
     function moveRight(sprite) {
-        var distance = (windowWidth - sprite['sprite'].offset().left);
+        var distance = windowWidth - sprite['sprite'].offset().left;
         sprite['sprite'].animate({left: windowWidth + 'px'}, distance / SPRITE_PIXELS_PER_SECOND * 1000, 'linear');
     }
 
@@ -20,6 +20,7 @@
      * Move the barbarian left
      */
     function moveLeft(sprite) {
+        console.log('moving ' + sprite['name'] + ' left');
         var distance = sprite['sprite'].offset().left;
         sprite['sprite'].animate({left: '0px'}, distance / SPRITE_PIXELS_PER_SECOND * 1000, 'linear');
     }
@@ -50,27 +51,25 @@ async function animateSprite(sprite, requestedAction, requestedDirection, times 
     var frames = sprite['frames'][requestedAction];
     var path = frames[sprite['direction']]['FRAMES'];
     var heightOffsetGridUnits = frames[sprite['direction']]['HEIGHT_OFFSET'];
+
     sprite['action'] = requestedAction;
     sprite['direction'] = requestedDirection;
+
     var heightOffset = heightOffsetGridUnits * sprite['sprite'].height();
     var index = 0;
     var iterations = times;
-    console.log('action ' + sprite['name'] + ' = ' + sprite['action'] + ' requested action = ' + requestedAction);
-    console.log('direction ' + sprite['name'] + ' = ' + sprite['direction'] + ' requested direct = ' + requestedDirection);
+    var windowWidth = $( document ).width() - sprite['sprite'].width();
+
     while(sprite['action'] === requestedAction && sprite['direction'] === requestedDirection) {
-       var windowWidth = $( document ).width() - sprite['sprite'].width();
        var position = path[index];
 
-       sprite['sprite'].css('background-position',(-1*position*sprite['sprite'].width()) + 'px ' + -1*heightOffset + 'px');
+       renderSpriteFrame(sprite, position, requestedAction);
        if (sprite['action'] === STOP) {
            break;
        }
-       if (sprite['direction'] === LEFT && sprite['sprite'].offset().left === 0) {
-           sprite['action'] = STOP;
-           break;
-       }
-       if (sprite['direction'] === RIGHT && sprite['sprite'].offset().left === windowWidth) {
-           console.log('direction is ' + sprite['direction']);
+       if (hitLeftBoundry(sprite) || hitRightBoundry(sprite)) {
+           // Since we are stopping set the frame to the stop frame (1st frame when walking)
+           renderSpriteFrame(sprite, 0, WALK);
            sprite['action'] = STOP;
            break;
        }
@@ -82,7 +81,6 @@ async function animateSprite(sprite, requestedAction, requestedDirection, times 
            if (times < 1 || --iterations > 0) {
                index = 0;
            } else {
-               console.log('d');
                break;
            }
        }
@@ -91,8 +89,24 @@ async function animateSprite(sprite, requestedAction, requestedDirection, times 
     // if we reach this point it means it was a terminating sprite animation, stop the movement if a new action has not
     // been started and reset the action so it can be repeated if desired. An exception is walking where a direction
     // change should not stop motion.
-    if (sprite['action'] !== WALK && sprite['action'] === requestedAction) {
+    if (sprite['action'] === requestedAction) {
         sprite['action'] = undefined;
         sprite['sprite'].stop();
+    }
+
+    function renderSpriteFrame(sprite, position, requestedAction) {
+        console.log('render sprite frame requested action is ' + requestedAction);
+        var frames = sprite['frames'][requestedAction];
+        var heightOffsetGridUnits = frames[sprite['direction']]['HEIGHT_OFFSET'];
+        var heightOffset = heightOffsetGridUnits * sprite['sprite'].height();
+        sprite['sprite'].css('background-position',(-1*position*sprite['sprite'].width()) + 'px ' + -1*heightOffset + 'px');
+    }
+
+    function hitLeftBoundry(sprite) {
+        return sprite['direction'] === LEFT && sprite['sprite'].offset().left === 0;
+    }
+
+    function hitRightBoundry(sprite) {
+        return sprite['direction'] === RIGHT && sprite['sprite'].offset().left === windowWidth;
     }
 }

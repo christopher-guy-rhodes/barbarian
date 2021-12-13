f8cd822884927186fb31b7cb75cb8857aa42716b
Christopher Rhodes <crhodes@amazon.com>
2021-12-13 00:27:20 -0800

    Cleaned up obstacle

--- js/messages/Messages.js
+++ js/messages/Messages.js
@@ -21,10 +21,18 @@ class Messages {
         ];
     }
 
+    /**
+     * Determine if the controls message is currently being displayed.
+     * @returns {boolean} true if the control message is showing, false otherwise
+     */
     isControlMessageDisplayed() {
         return this.controlMessage.css(CSS_DISPLAY_LABEL) === CSS_BLOCK_LABEL;
     }
 
+    /**
+     * Display the sound toggle message on the screen.
+     * @param isSoundOn whether the sound is currently on or not
+     */
     showSoundToggleMessage(isSoundOn) {
         this.hideAllMessages();
         (isSoundOn ? this.soundOnMessage : this.soundOffMessage).css('display', 'none');
@@ -36,32 +44,54 @@ class Messages {
 
     }
 
+    /**
+     * Display the message. Hides all other messages.
+     * @param message the element of the message to display
+     */
     showMessage(message) {
         $.each(this.messages, function(idx, e) {
             e.css('display', e[0].classList !== message[0].classList ? 'none' : 'block');
         });
     }
 
+    /**
+     * Show the pause message.
+     */
     showPauseMessage() {
         this.showMessage(this.pauseMessage);
     }
 
+    /**
+     * Show that start message.
+     */
     showStartMessage() {
         this.showMessage(this.startMessage);
     }
 
+    /**
+     * Show the game over message.
+     */
     showGameOverMessage() {
         this.showMessage(this.gameOverMessage);
     }
 
+    /**
+     * Show the game won message.
+     */
     showGameWonMessage() {
         this.showMessage(this.gameWonMessage);
     }
 
+    /**
+     * Show the control message.
+     */
     showControlMessage() {
         this.showMessage(this.controlMessage);
     }
 
+    /**
+     * Hide all messages.
+     */
     hideAllMessages() {
         $.each(this.messages, function(idx, e) {
             e.css('display', 'none');
--- js/obstacles/Obstacle.js
+++ js/obstacles/Obstacle.js
@@ -3,19 +3,7 @@ const JUMP_EVADE_THRESHOLD = 4;
 class Obstacle {
 
     constructor(x, y, type, action, minJumpThreshold, maxJumpThreshold) {
-
-        if (x === undefined) {
-            throw new Error('Obstacle: x is a required parameter');
-        }
-        if (y === undefined) {
-            throw new Error('Obstacle: y is a required parameter');
-        }
-        if (type === undefined) {
-            throw new Error('Obstacle: type is a required parameter');
-        }
-        if (action === undefined) {
-            throw new Error('Obstacle: action is a required parameter');
-        }
+        validateRequiredParams(this.constructor, arguments, 'x', 'y', 'type', 'action');
         if (minJumpThreshold !== undefined && maxJumpThreshold === undefined ||
             maxJumpThreshold !== undefined && minJumpThreshold === undefined) {
             throw new Error('Obstacle: both the min and max jump thresholds must be both included or both omitted');
@@ -35,60 +23,129 @@ class Obstacle {
         }
     }
 
+    /**
+     * Determine if the character is stopped by a boundary.
+     * @param character the character
+     * @param gameBoard the game board
+     * @returns {boolean} true if the character is stopped by a boundary, false otherwise
+     */
     static isStoppedByBoundary(character, gameBoard) {
         return !this.isAtWaterBoundary(character, gameBoard) && Obstacle.isAtBoundary(character, gameBoard);
     }
 
+    /**
+     * Determine if the character is past another character.
+     * @param character the character
+     * @param otherCharacter the character to check if the character has passed
+     * @returns {boolean} true if character has passed otherCharacter, false otherwise
+     */
+    static isPastCharacter(character, otherCharacter) {
+        return this.isPastCharacterLeft(character, otherCharacter) ||
+            this.isPastCharacterRight(character, otherCharacter);
+    }
+
+    /**
+     * Determine whether the character is at the boundary.
+     * @param character the character
+     * @param gameBoard the game board
+     * @returns {boolean} true if the character is at the boundary, false otherwise
+     */
+    static isAtBoundary(character, gameBoard) {
+        validateRequiredParams(this.isAtBoundary, arguments, 'gameBoard');
+        return Obstacle.isCharacterAtLeftBoundary(character) || Obstacle.isCharacterAtRightBoundary(character);
+    }
+
+    /**
+     * Get the x coordinate of the obstacle.
+     * @returns {number} the x coordinate
+     */
+    getX() {
+        return this.x;
+    }
+
+    /**
+     * Determine if the obstacle is a traversable downhill elevation.
+     * @param character the character attempting to traverse the obstacle
+     * @returns {boolean} true if the elevation is traversable and downhill, false otherwise
+     */
+    isTraversableDownhillElevation(character) {
+        return this.isDownHillFrom(character.getY()) || (!character.isBarbarian()
+            && character.getProperties().getCanElevate());
+    }
+
+    /**
+     * Get the type of obstacle.
+     * @returns {string} the obstacle type
+     */
+    getType() {
+        return this.type;
+    }
+
+    /**
+     * Determine if this obstacle is an elevation.
+     * @returns {boolean} true if this obstacle is an elevation, false otherwise.
+     */
+    getIsElevation() {
+        return this.type === ELEVATION_LABEL;
+    }
+
+    /**
+     * Determine if the obstacle is a pit.
+     * @returns {boolean} true if the obstacle is a pit, false otherwise
+     */
+    getIsPit() {
+        return this.type === OBSTACLE_PIT_LABEL;
+    }
+
+    /**
+     * Get the obstacle height.
+     * @returns {number} the obstacle height
+     */
+    getHeight() {
+        return this.y;
+    }
+
+    /* private */
     static isAtWaterBoundary(character, gameBoard) {
         return !gameBoard.isScrollAllowed(character.getScreenNumber(), character.getHorizontalDirection()) &&
             gameBoard.isWater(character.getScreenNumber());
     }
 
-    static isPastCharacter(character, otherCharacter) {
-        return this.isPastCharacterLeft(character, otherCharacter) ||
-            this.isPastCharacterRight(character, otherCharacter);
-    }
-s
+    /* private */
     static isPastCharacterLeft(character, otherCharacter) {
         return character.isFacingLeft()
             && character.getX() + character.getWidth() * PASSING_MULTIPLIER < otherCharacter.getX() ||
                 Obstacle.isCharacterAtLeftBoundary(character);
     }
 
+    /* private */
     static isPastCharacterRight(character, otherCharacter) {
         return character.isFacingRight() &&
             character.getX() - character.getWidth() * PASSING_MULTIPLIER > otherCharacter.getX() ||
                 Obstacle.isCharacterAtRightBoundary(character);
     }
 
+    /* private */
     static isCharacterAtLeftBoundary(character) {
         return !character.isFacingRight() && character.getX() === 0;
     }
 
+    /* private */
     static isCharacterAtRightBoundary(character) {
         return !character.isFacingLeft()
             && character.getX() === SCREEN_WIDTH - character.getProperties().getSprite().width();
     }
 
-    static isAtBoundary(character, gameBoard) {
-        validateRequiredParams(this.isAtBoundary, arguments, 'gameBoard');
-        return Obstacle.isCharacterAtLeftBoundary(character) || Obstacle.isCharacterAtRightBoundary(character);
-    }
-
+    /* private */
     isCloseButNotPast(x, direction) {
         if (direction === RIGHT_LABEL) {
-            //return this.x  > x - 25;
             return x - 50 < this.x;
         } else {
-            //return this.x < x + 25;
             return x + 50 > this.x;
         }
     }
 
-    getX() {
-        return this.x;
-    }
-
+    /* private */
     isPast(x, direction) {
         if (direction === RIGHT_LABEL) {
             return x > this.x;
@@ -97,82 +154,55 @@ s
         }
     }
 
+    /* private */
     isDownHillFrom(y) {
         return this.y <= y && this.type !== OBSTACLE_PIT_LABEL;
     }
 
-    isEvadedWithJumpAt(x) {
-        if (this.jumpThresholds === undefined) {
-            return false;
-        }
-        return x > this.jumpThresholds[MIN_LABEL] && x < this.jumpThresholds[MAX_LABEL];
-    }
-
-    isTraversableDownhillElevation(character) {
-        return this.isDownHillFrom(character.getY()) || (!character.isBarbarian()
-            && character.getProperties().getCanElevate());
-    }
-
+    /* private */
     hitElevation(character) {
         return this.getIsElevation() && character.getY() != this.getHeight();
     }
 
+    /* private */
     isCharacterPastObstacle(character) {
         return !character.isDead() && this.isPast(character.getX(), character.getHorizontalDirection());
     }
 
+    /* private */
     filterIfCharacterAvoided(character) {
         return this.didCharacterHitObstacle(character) ? this : undefined;
     }
 
+    /* private */
     didCharacterHitObstacle(character) {
         return this.isCharacterPastObstacle(character) &&
             (this.didCharacterHitElevation(character) || this.didBarbarianFallInPit(character));
     }
 
+    /* private */
     didCharacterHitElevation(character) {
         return this.hitElevation(character) && (!character.isBarbarian() || !character.isAction(ATTACK_LABEL));
     }
 
+    /* private */
     didJumpEvadePit(character) {
         let frame = character.getCurrentFrame(JUMP_LABEL);
         // The Barbarian must jump from the edge of the pit which puts him at jump frame PIT_JUMP_EVADE_FRAME
         return character.getAction() === JUMP_LABEL && frame === PIT_JUMP_EVADE_FRAME;
     }
 
+    /* private */
     didBarbarianFallInPit(character) {
         return this.getIsPit() && character.isBarbarian() && !this.didJumpEvadePit(character)
             && !character.isAction(FALL_LABEL);
     }
 
-    getFailAction() {
-        return this.action;
-    }
-
-    getType() {
-        return this.type;
-    }
-
-    getIsElevation() {
-        return this.type === ELEVATION_LABEL;
-    }
-
-    getIsPit() {
-        return this.type === OBSTACLE_PIT_LABEL;
-    }
-
-    getHeight() {
-        return this.y;
-    }
-
-    getMinJumpThreshold() {
-        return this.jumpThresholds[MIN_LABEL];
-    }
-
-    getMaxJumpThreshold() {
-        return this.jumpThresholds[MAX_LABEL];
-    }
-
+    /**
+     * Determine if the character evaded the obstacle with a jump.
+     * @param character the character
+     * @returns {boolean} true if the character evaded the obstacle with a jump, false otherwise
+     */
     didCharacterJumpEvade(character) {
         return character.getAction() === JUMP_LABEL && character.getCurrentFrame(JUMP_LABEL) < JUMP_EVADE_THRESHOLD;
     }
